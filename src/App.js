1ba51720b4d14701bfb436ecf228be74cee34351
finePine <lsm3645@g.skku.edu>
2023-06-27 19:14:50 +0900

    feat: add api fetching functions using axios and react-query

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Page1 from "./pages/Page1/Page1";
 import Page2 from "./pages/Dashboard/Dashboard";
 import Page3 from "./pages/Page3/Page3";
 import NotFound from "./pages/NotFound/NotFound";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 import "./App.css";
 
@@ -13,67 +15,71 @@ import Sidebar from "./components/Sidebar";
 import Top1 from "./components/Top1";
 import Top2 from "./components/Top2";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Login />
-            </>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <>
-              <Login />
-            </>
-          }
-        />
-        <Route
-          path="/page1"
-          element={
-            <>
-              <Top1 />
-              <Top2 />
-              <Sidebar>
-                <div style={{ borderTopLeftRadius: "10px" }}>
-                  <Page1 />
-                </div>
-              </Sidebar>
-            </>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <>
-              <Top1 />
-              <Top2 />
-              <Sidebar>
-                <Page2 />
-              </Sidebar>
-            </>
-          }
-        />
-        <Route
-          path="/page3"
-          element={
-            <>
-              <Top1 />
-              <Top2 />
-              <Sidebar>
-                <Page3 />
-              </Sidebar>
-            </>
-          }
-        />
-        <Route path="*" element={<NotFound />} /> {/* catch-all route */}
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Login />
+              </>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <>
+                <Login />
+              </>
+            }
+          />
+          <Route
+            path="/page1"
+            element={
+              <>
+                <Top1 />
+                <Top2 />
+                <Sidebar>
+                  <div style={{ borderTopLeftRadius: "10px" }}>
+                    <Page1 />
+                  </div>
+                </Sidebar>
+              </>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <>
+                <Top1 />
+                <Top2 />
+                <Sidebar>
+                  <Page2 />
+                </Sidebar>
+              </>
+            }
+          />
+          <Route
+            path="/page3"
+            element={
+              <>
+                <Top1 />
+                <Top2 />
+                <Sidebar>
+                  <Page3 />
+                </Sidebar>
+              </>
+            }
+          />
+          <Route path="*" element={<NotFound />} /> {/* catch-all route */}
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
