290db1ee1d59025b7d69ba229390cbd22ec6a372
repsevaivulpsunos <yoonji4024@g.skku.edu>
2023-06-28 16:10:49 +0900

    Change the name of Page1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
-import Page1 from "./pages/Page1/Page1";
+import Page1 from "./pages/Giving/Giving";
 import Page2 from "./pages/Dashboard/Dashboard";
 import Page3 from "./pages/Page3/Page3";
 import NotFound from "./pages/NotFound/NotFound";
