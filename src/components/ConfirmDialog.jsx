e9970ab850365ee9fc4093672c788e052396b043
finePine <lsm3645@g.skku.edu>
2023-07-20 13:11:33 +0900

    feat: Add comfirmation dialog

--- src/components/ConfirmDialog.jsx
+++ src/components/ConfirmDialog.jsx
@@ -8,21 +8,14 @@ import {
 } from "@mui/material";
 import { MdInfoOutline } from "react-icons/md";
 
-function ConfirmDialog({ open, desc, handleConfirm, handleCancel }) {
+function ConfirmDialog({ open, children, handleConfirm, handleCancel }) {
   return (
-    <Dialog
-      open={open}
-      onClose={handleCancel}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-    >
-      <DialogTitle id="alert-dialog-title">
+    <Dialog open={open} onClose={handleCancel}>
+      <DialogTitle>
         <MdInfoOutline />
       </DialogTitle>
       <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          {desc}
-        </DialogContentText>
+        <DialogContentText>{children}</DialogContentText>
       </DialogContent>
       <DialogActions>
         <Button variant="contained" color="primary" onClick={handleConfirm}>
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -12,9 +12,11 @@ import {
   AccordionSummary,
   Typography,
   AccordionDetails,
+  Button,
 } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import CustomPagination from "../../components/CustomPagination";
+import ConfirmDialog from "../../components/ConfirmDialog";
 
 export const UserPointGraph = ({ details }) => {
   return (
@@ -89,26 +91,46 @@ export const UserPointHistory = ({ details }) => {
 };
 
 const KoinLink = ({ pfInfo }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleConfirm = (url) => {
+    // window.location.href = {url};
+    window.open(url, "_blank");
+    setOpen(false);
+  };
+
   return (
-    <a
-      key={pfInfo.pf_name}
-      href={pfInfo.pf_link}
-      target="_blank"
-      className="flex justify-center items-center gap-1 w-[258px] h-[156px] bg-background rounded-lg border-solid border-2 border-primary hover:bg-primary hover:text-onPrimary hover:shadow-lg hover:scale-[1.03] transition-all"
-    >
-      {pfInfo.pf_logo ? (
-        <>
-          <img
-            className="w-8 h-8"
-            src={pfInfo.pf_logo}
-            alt={pfInfo.pf_name + " 링크"}
-          />
-          <div className="text-label-l">{pfInfo.pf_name}</div>
-        </>
-      ) : (
-        <div className="font-gugi text-xl">{pfInfo.pf_name}</div>
-      )}
-    </a>
+    <>
+      <ConfirmDialog
+        open={open}
+        handleConfirm={() => {
+          handleConfirm(pfInfo.pf_link);
+        }}
+        handleCancel={() => {
+          setOpen(false);
+        }}
+      >{`${pfInfo.pf_name}으로 이동하시겠습니까?`}</ConfirmDialog>
+      <Button
+        key={pfInfo.pf_name}
+        onClick={() => {
+          setOpen(true);
+        }}
+        className="flex justify-center items-center gap-1 w-[258px] h-[156px] bg-background rounded-lg border-solid border-2 border-primaryhover:shadow-lg hover:scale-[1.03] transition-all"
+      >
+        {pfInfo.pf_logo ? (
+          <>
+            <img
+              className="w-8 h-8"
+              src={pfInfo.pf_logo}
+              alt={pfInfo.pf_name + " 링크"}
+            />
+            <div className="text-label-l">{pfInfo.pf_name}</div>
+          </>
+        ) : (
+          <div className="font-gugi text-xl">{pfInfo.pf_name}</div>
+        )}
+      </Button>
+    </>
   );
 };
 
