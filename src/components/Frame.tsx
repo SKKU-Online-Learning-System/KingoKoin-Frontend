2bec87eb4056fcd24de172b4f94c449632b6404b
finePine <lsm3645@g.skku.edu>
2023-07-30 21:35:02 +0900

    feat: Header, Footer, Frame 추가

---
+++ src/components/Frame.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Frame = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Frame;
