9df0c2aef9cb1cfa83827eba5a2ff849c7cc9cef
finePine <lsm3645@g.skku.edu>
2023-08-01 02:29:39 +0900

    feat: Status 컴포넌트 추가

---
+++ src/components/feedback/NoData.tsx
@@ -0,0 +1,9 @@
+const NoData = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-2 w-full h-full">
+      <p className="text-darkGray">보여줄 데이터가 존재하지 않습니다.</p>
+    </div>
+  );
+};
+
+export default NoData;
---
+++ src/components/feedback/UnknownError.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@mui/material";
+
+interface ErrorProps {
+  error: Error;
+}
+
+const UnknownError = ({ error }: ErrorProps) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-2 w-full h-full">
+      <h1 className="text-display">오류가 발생했습니다.</h1>
+      <caption className="text-darkGray">{error.message}</caption>
+      <div className="text-center">
+        <p>요청사항을 처리하는데 실패했습니다.</p>
+        <p>오류가 계속 발생한다면 이메일을 통해 문의해주시기 바랍니다.</p>
+      </div>
+      <Button
+        variant="contained"
+        onClick={() => {
+          window.history.back();
+        }}
+      >
+        되돌아가기
+      </Button>
+    </div>
+  );
+};
+
+export default UnknownError;
